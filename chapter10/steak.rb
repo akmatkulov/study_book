bed1974bb9944556d4ac074d2400322905968f5c
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-06-28 06:51:32 +0300

    Fixed steak code

--- chapter10/steak.rb
+++ chapter10/steak.rb
@@ -1,21 +1,32 @@
 class Steak
 
+    include Comparable 
+
     GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
 
     attr_accessor :grade 
     
-    def >(other)
-        GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
+    def <=>(other)
+        if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
+            return -1 
+        elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
+            return 0
+        else 
+            return 1     
+        end
     end 
 end 
 
-first_steak = Steak.new 
-first_steak.grade = "Prime"
+prime = Steak.new 
+prime.grade = "Prime"
+choice = Steak.new 
+choice.grade = "Choice"
+select1 = Steak.new 
+select1.grade = "Select"
 
-second_steak = Steak.new 
-second_steak.grade = "Choice"
-
-if first_steak > second_steak
-    puts "I'll take #{first_steak.inspect}"
-end 
-
+puts "prime > choice: #{prime > choice}"
+puts "prime < select: #{prime < select1}"
+puts "select == select: #{select1 == select1}"
+puts "select <= select: #{select1 <= select1}"
+print "choice.between?(select, prime): "
+puts choice.between?(select1, prime)
